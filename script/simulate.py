c43a39eab7afd00bcfd865330a384e13b980641e
Yushi Yang <yangyushi1992@icloud.com>
2020-04-28 20:29:39 +0100

    new benchmark
    The agreement between C++ code and Python code proved the neighbour list works fine

--- script/simulate.py
+++ script/simulate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import sys
 sys.path.append('../lib')
@@ -8,25 +9,24 @@ import csimulate
 
 
 N = 200
-density = 1.0
+density = 5.0
 box = (N / density) ** (1/3)
 spd = 0.1
 r = 1.0
-noises = np.arange(0.0, 1.2, 0.2)
+noises = np.arange(0.0, 1.0, 0.05)
 
 orders = []
 
 t0 = time.time()
-for reduced_noise in noises:
-    noise = reduced_noise * np.sqrt(density)
-    result = csimulate.vicsek_3d_pbc(N, box, noise, spd, r, 2000, 2000)  # (T, n, 6)
+for noise in noises:
+    result = csimulate.vicsek_3d_pbc(N, box, noise, spd, r, 1000, 1000)  # (T, n, 6)
     order = np.sqrt((result[:, :, 3:].mean(1) ** 2).sum(-1)).mean()
     orders.append(order / spd)
 t_cpp = time.time() - t0
 
 # plotting results from a Python code
 result_py = np.load('result_py.npy')
-plt.scatter(*result_py, color='tomato', s=64, facecolor='none', label=f'Python: ~280s')
+plt.scatter(*result_py, color='tomato', s=64, facecolor='none', label=f'Python: ~467s')
 
 # plotting C++ results
 plt.scatter(noises, orders, color='teal', marker='+', label=f'C++: {t_cpp:.2f}s', s=72)
