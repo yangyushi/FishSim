cd3e7af32448c5f0d1ce2ab6a991696c6be510ef
Yushi Yang <yangyushi1992@icloud.com>
2020-05-14 00:24:08 +0100

    minor bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,23 @@ int main(){
     Attanasi2014PCB system{
         100, 1.0, 0.05, 0.02, 0.03,
     };
+    system.dump("test.xyz");
 
     cout << "system created" << endl;
 
-    for (int step=0; step < 10000; step++){
+    for (int step=0; step < 500; step++){
         if (step % 20 == 0){
             system.move(true);
         }
         else {
             system.move(false);
         }
+        if (step % jump == 0) system.dump("test.xyz");
     }
 
     cout << "movement works" << endl;
 
-    for (int step=0; step < 100; step++){
+    for (int step=0; step < 500; step++){
         if (step % 20 == 0){
             system.move(true);
         }
