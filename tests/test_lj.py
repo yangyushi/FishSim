a7001fd167450af07338cef4b8d1f7744f6f9445
Yushi Yang <yy17363@bristol.ac.uk>
2021-09-01 14:25:31 +0100

    adding test code

---
+++ tests/test_lj.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+sys.path.insert(0, '../lib')
+import fish_sim as fs
+
+
+def test_lj():
+    N, dim = 256, 3
+    density = 0.75
+    block = 100
+    box = (N / density) ** (1 / dim)
+
+    system_lj = fs.model.BDLJPBC(
+        N, dim, box=box, dt=0.005,
+        gamma=1, kT=1, m=1
+    )
+
+    system_lj.r = np.loadtxt('cnf.inp', skiprows=2, usecols=[0, 1, 2])
+    system_lj.v = np.loadtxt('cnf.inp', skiprows=2, usecols=[3, 4, 5])
+
+    obs = fs.utility.Thermodynamic(block=block)
+    system_lj.attach(obs)
+
+    fs.utility.animate(
+        system_lj, r=10, jump=2, show=True, frames=100, repeat=False,
+    )
+
+
+if __name__ == "__main__":
+    test_lj()
