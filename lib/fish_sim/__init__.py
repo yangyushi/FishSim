e6c99860934d2dcc2a994d424118667873d4e8b5
Yushi Yang <yangyushi1992@icloud.com>
2021-09-04 22:11:58 +0100

    ignore `cmodel` if import failed

--- lib/fish_sim/__init__.py
+++ lib/fish_sim/__init__.py
@@ -1,3 +1,11 @@
 from . import utility
-from . import cmodel
 from . import model
+try:
+    from . import cmodel
+except ImportError:
+    import warnings
+    warnings.warn(
+        "failed to import the cmodel module, if you don't need \
+        the fast model in written C++ you can ignore this warning.",
+        ImportWarning
+    )
