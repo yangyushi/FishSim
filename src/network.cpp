fc4b1f88bf637505a8dbd6b41723d93ec3b23940
Yushi Yang <yangyushi1992@icloud.com>
2021-09-06 15:12:38 +0100

    update the network with graphs

--- src/network.cpp
+++ src/network.cpp
@@ -3,9 +3,48 @@
 std::random_device rd;
 std::mt19937 g(rd());
 
+Graph::Graph(int n){
+    for (int i = 0; i < n; i++){
+        nodes_.push_back(i);
+    }
+}
+
+Conn Graph::as_connections(){
+    Conn connections {};
+    for (int i = 0; i < this->size(); i++){
+        connections.push_back(Nodes{});
+    }
+    for (auto edge : edges_){
+        connections[edge[0]].push_back(edge[1]);
+    }
+    return connections;
+}
+
+ConnMat Graph::as_matrix(){
+    int n = this->size();
+    ConnMat mat{n, n};
+    mat.setZero();
+    for (auto edge : edges_){
+        mat(edge[0], edge[1]) += 1;
+    }
+    return mat;
+}
+
+
+Graph random_regular_graph(int d, int n){
+    if (n * d % 2 == 0){
+        throw("In valid argument, n * d shuold be even");
+    }
+    if (not (0 <= d <= n)) {
+        throw("the 0 <= d < n inequality must be satisfied");
+    }
+    if (d == 0) {return Graph(n);}
+    return Graph(n);
+}
+
 
 Network3D::Network3D(int n, int k, double eta)
-    : AVS3D{n, eta, 1.0}, k_(k) {
+    : AVS3D{n, eta, 1.0}, k_(k), graph_{n} {
         for (int i = 0; i < n; i++){
             indices_.push_back(i);
         }
@@ -49,3 +88,4 @@ void Network3D::evolve(int steps){
         this->move();
     }
 }
+
--- src/network.hpp
+++ src/network.hpp
@@ -1,9 +1,24 @@
 #ifndef NETWORK
 #define NETWORK
 
-#include <vector>
-#include <Eigen/Dense>
+#include <algorithm>
 #include "core.hpp"
+
+using Edges = Indices2D;  // vector < array< int, 2 > >
+using Nodes = std::vector< int >;
+
+
+struct Graph {
+    Edges edges_;
+    Nodes nodes_;
+    Graph(int n);
+    Conn as_connections();
+    ConnMat as_matrix();
+    inline int size() {return *std::max_element(nodes_.begin(), nodes_.end()) + 1;}
+};
+
+Graph random_regular_graph(int d, int n);
+
 
 /*
  * The vectorial network model introduced by Aldana and Huepe in 2003
@@ -13,16 +28,13 @@ class Network3D : public AVS3D{
         int k_; // connection number 
         std::vector<int> indices_;
         int dynamic_ = 0;  // 0 - quenched dynamic; 1 - annealed dynamic
+        Graph graph_;
 
     public:
         Network3D(int n, int k, double eta);
         void random_align();
         void move();
         void evolve(int steps);
-
-};
-
-class TCNetwork3D : public Network3D{
 };
 
 #endif
