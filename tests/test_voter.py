f996c0a1005cd5f6d462c415daca2b9ccedd008e
Yushi Yang <yangyushi1992@icloud.com>
2021-09-15 13:57:10 +0100

    test the voter model
    the simulation result is compared against the mean-field prediction

---
+++ tests/test_voter.py
@@ -0,0 +1,28 @@
+import fish_sim as fs
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def test_voter(n, steps=100):
+    k = 3
+    eta_vals = np.linspace(0, 1, 10)
+    magnitisation = np.empty(10)
+    for i, eta in enumerate(eta_vals):
+        system = fs.cmodel.Voter(n, k, eta)
+        system.evolve(steps)
+        mag_vals = np.empty(steps)
+        for j in range(steps):
+            system.move(True)
+            mag_vals[j] = np.abs(system.get_magnetisation())
+        magnitisation[i] = mag_vals.mean()
+    return eta_vals, magnitisation
+
+
+if __name__ == "__main__":
+    eta, mag = test_voter(n=10000, steps=100)
+    eta_theory = np.linspace(0, 1, 100)
+    mag_theory = (3 - 2 / (1 - eta_theory)) ** 0.5
+    mag_theory[eta_theory > 1/3] = 0
+    plt.scatter(eta, mag)
+    plt.plot(eta_theory, mag_theory)
+    plt.show()
