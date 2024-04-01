16a82010d5fe99b4890eb3bd4c4f077a447ddf38
yangyushi <yangyushi1992@icloud.com>
2024-04-01 09:03:09 +0800

    make voter test being checked against theory; change wrong file mode of vicsek phase

--- tests/test_voter.py
+++ tests/test_voter.py
@@ -1,13 +1,12 @@
 import pytest
 import fish_sim as fs
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 @pytest.mark.parametrize("n", [10, 50])
 def test_voter(n, steps=100):
     k = 3
-    eta_vals = np.linspace(0, 1, 10)
+    eta_vals = np.linspace(0.01, 0.99, 10)
     magnitisation = np.empty(10)
     for i, eta in enumerate(eta_vals):
         system = fs.cmodel.Voter(n, k, eta)
@@ -17,14 +16,12 @@ def test_voter(n, steps=100):
             system.move(True)
             mag_vals[j] = np.abs(system.get_polarisation())
         magnitisation[i] = mag_vals.mean()
-    return eta_vals, magnitisation
+
+    tmp = 3 - 2 / (1 - eta_vals)
+    tmp[tmp < 0] = 0
+    theory = (tmp) ** 0.5
+    assert (theory - magnitisation).mean() < 1e-2
 
 
 if __name__ == "__main__":
-    eta, mag = test_voter(n=10000, steps=100)
-    eta_theory = np.linspace(0, 1, 100)
-    mag_theory = (3 - 2 / (1 - eta_theory)) ** 0.5
-    mag_theory[eta_theory > 1/3] = 0
-    plt.scatter(eta, mag)
-    plt.plot(eta_theory, mag_theory)
-    plt.show()
+    test_voter(n=2000, steps=100)
